6aca5eb4c0159286b820c68a6e3e7dfa6a4e7049
alin <alin@fpcusa.com>
2018-12-20 14:23:16 -0500

    initial commit

---
+++ src/components/Frame.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class Frame extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMenu: false
+        };
+    }
+
+    handleHamburgerClick = () => {
+        this.setState({
+            showMenu: !this.state.showMenu
+        });
+    }
+
+    render() {
+        const { children } = this.props;
+        // const langPath = this.props.lang;
+        // const lang = langPath || "en";
+        // const relPath = getRelPath(langPath, this.props.location.pathname);
+        const viewPortWidth = window.innerWidth;
+        return (
+            <div>
+                <header>This is header</header>
+                <hr />
+                <section>
+                    {children}
+                </section>
+                <hr />
+                <footer>
+                2019@FPC                    
+                <br/> 
+                ( viewPortWidth: {viewPortWidth} px )                
+                </footer>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import About from './components/About';
+import Frame from './components/Frame';
+import Home from './components/Home';
+import Links from './components/Link';
+// import Landing from './components/Landing';
+import Photos from './components/Photo';
+
+import { HashRouter, Route, Link } from "react-router-dom"
+
 import * as serviceWorker from './serviceWorker';
+
+// const components = Object.freeze({
+//     frame: Frame,
+//     landing: Landing,
+//     topPages: [
+//         { path: "home", component: Home },
+//         { path: "about", component: About },
+//         { path: "link", component: Links },
+//         { path: "photo", component: Photos }
+//     ]
+// });
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                    </ul>
+                    <hr />
+                    <Frame>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/link" component={Links} />
+                        <Route path="/photo" component={Photos} />
+                    </Frame>
+
+
+
+                </div>
+            </HashRouter>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
