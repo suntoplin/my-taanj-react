495a565556e592524af7f9b7f03176e21a2fbd30
alin <alin@fpcusa.com>
2019-02-01 15:56:28 -0500

    add react-slick

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
+import Slider from "react-slick";
+
 import Button from "muicss/lib/react/button";
 import Container from 'muicss/lib/react/container';
 import Divider from 'muicss/lib/react/divider';
@@ -28,9 +30,38 @@ export default class Home extends React.PureComponent {
         const viewPortWidth = window.innerWidth;
         const isMobile = viewPortWidth < 600;
 
+        const settings = {
+            dots: true,
+            infinite: true,
+            lazyLoad: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000
+        };
+
         return (
             <div>
-            
+                <Container>
+                    <Row>
+                        <Col md={12}>
+                            <h2> Single Item</h2>
+                            <Slider {...settings}>
+                                {
+                                    HomeData.events[0].photos.map(
+                                        (image, i) =>
+                                            <div key={i}>
+                                                <img src={require(`../${image.img}`)} alt={"test"} />
+                                                <h3>{image.img}</h3>
+                                            </div>
+                                    )
+                                }
+                            </Slider>
+                            <hr />
+                        </Col>
+                    </Row>
+                </Container>
+
                 <Container>
                     {HomeData.events.map((event, id) =>
                         <Row key={id}>
