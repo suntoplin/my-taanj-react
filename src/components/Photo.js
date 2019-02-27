7fcf353fbdd2dbecce2b918349c0642381f034a1
alin <alin@fpcusa.com>
2019-02-27 11:39:55 -0500

    changed to funtional components

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-
-// import Slider from "react-slick";
+import React, { Fragment } from 'react';
 
 import Button from "muicss/lib/react/button";
 import Container from 'muicss/lib/react/container';
@@ -11,40 +8,33 @@ import Col from 'muicss/lib/react/col';
 
 import HomeData from '../data/HomeData';
 
-// functional component
 function Image(props) {
     const { source } = props;
-    return <LazyLoadImage alt={" "} effect="blur" width={`100%`} src={require(`../${source}`)} />;
+    return <img alt={" "} width={`100%`} src={require(`../${source}`)} />;
 }
 
-export default class Photos extends React.Component {
-    render() {
-        const viewPortWidth = window.innerWidth;
-        const isMobile = viewPortWidth < 600;
-        return (
-            <div>
-                <Container>
-                    {HomeData.events.map((event, id) =>
-                        <Row key={id}>
-                            <Col md={12}>Title: {event.date}</Col>
-                            {event.photos.map((image, i) =>
-                                <Col
-                                    key={i}
-                                    md={isMobile ? 12 : 3}
-                                >
-                                    <Image source={image.img} />
-                                </Col>
-                            )}
-                            <Col md={12}>
-                                <a href={event.link} target={"_blank"}>
-                                    <Button color="primary">MORE</Button>
-                                </a>
-                            </Col>
-                            <Divider />
-                        </Row>
-                    )}
-                </Container>
-            </div>
-        );
-    }
+function Albums(props) {
+    const { photos, isMobile } = props;
+    return <div>{photos.map((image, i) => <Col key={i} md={isMobile ? 12 : 3}><Image source={image.img} /></Col>)}</div>;
+}
+
+export default function Photos() {
+    const viewPortWidth = window.innerWidth;
+    const isMobile = viewPortWidth < 600;
+    return (
+        <Container>
+            {HomeData.events.map((event, id) =>
+                <Row key={id}>
+                    <Col md={12}>
+                        Title: {event.date}
+                    </Col>
+                    <Albums photos={event.photos} isMobile={isMobile} />
+                    <Col md={12}>
+                        <a href={event.link} target={"_blank"}><Button color="primary">MORE</Button></a>
+                    </Col>
+                    <Divider />
+                </Row>
+            )}
+        </Container>
+    );
 }
